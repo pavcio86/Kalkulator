c69610722538b0d74f19a4455b5fe0beba248dbc
Pawel <pavcio86developer@onet.pl>
2023-03-29 23:15:31 +0200

    First Commit

---
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.kalkulator;
+
+public class Calculator {
+    public double subtraction (double a, double b){
+        return a - b;
+    }
+
+    public double addition (double a, double b){
+        return a + b;
+    }
+}
